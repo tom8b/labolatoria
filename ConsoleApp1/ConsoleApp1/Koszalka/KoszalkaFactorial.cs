1d9d078c7e8d48b3f29e71da8bd8b61a26ef643b
Bartosz Koszalka <38052869+koshi37@users.noreply.github.com>
2020-04-27 21:56:32 +0200

    Added files for Koszalka

---
+++ ConsoleApp1/ConsoleApp1/Koszalka/KoszalkaFactorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Koszalka
+{
+    class KoszalkaFactorial : IFactorial
+    {
+        public long CountFactorial(int number)
+        {
+            long result = 1;
+            if (number > 0)
+                for (int i = 1; i <= number; i++)
+                    result *= i;
+            else if (number == 0)
+                result = 1;
+            else
+                throw new System.ArgumentException("Parameter cannot be < 0");
+
+            Console.WriteLine($"Silnia dla {x} ma wartosc: {result}.");
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Koszalka/KoszalkaFibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Koszalka
+{
+    class KoszalkaFibonacci : IFibonacci
+    {
+        public int CountValueOfElement(int elementNumber)
+        {
+            int result = 1;
+
+            if (elementNumber == 1 || elementNumber == 2)
+                result = 1;
+            else if (elementNumber <= 0)
+            {
+                throw new System.ArgumentException("Parameter cannot be <= 0");
+            }
+            else
+            {
+                int f1 = 1;
+                int f2 = 1;
+                int temp;
+                for (int i = 3; i <= elementNumber; i++)
+                {
+                    temp = f1 + f2;
+                    f1 = f2;
+                    f2 = temp;
+                }
+                result = f2;
+            }
+
+            Console.WriteLine($"{elementNumber} element w ciagu Fibonacciego ma wartosc: {result}.");
+            return result;
+        }
+    }
+}
+
+
